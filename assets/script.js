da485c4afc9f77b9ba3684c3d62132dba49de19f
Arifals <halo.langit21@gmail.com>
2024-10-04 19:19:53 +0700

    mini update

--- assets/script.js
+++ assets/script.js
@@ -89,6 +89,7 @@ const showPetunjuk = () => {
   const guides = document.getElementById("petunjuk");
   guides.classList.add("show");
 };
+
 const closePetunjuk = () => {
   const guides = document.getElementById("petunjuk");
   guides.classList.remove("show");
